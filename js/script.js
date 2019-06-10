c238eeb178ab57fb2661c3da9f66c132a917d285
eniciodehaas <43574607+eniciodehaas@users.noreply.github.com>
2019-06-10 20:19:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// script submenu
+
+const heeftSubmenu = document.querySelectorAll('.menu-item-has-children');
+
+for (let i = 0; i < heeftSubmenu.length; i++) {
+  const submenu     = heeftSubmenu[i].querySelector('.sub-menu');
+  const linkSubmenu = heeftSubmenu[i].querySelector('a');
+  // submenu verbergen
+  submenu.classList.toggle('menu-verbergen');
+
+  // submenu verschijnen
+  linkSubmenu.addEventListener('click', (e) => {
+    e.preventDefault();
+    submenu.classList.toggle('menu-verbergen');
+    linkSubmenu.classList.toggle('menu-open');
+  })
+}
